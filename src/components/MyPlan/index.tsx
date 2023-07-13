b341642c08a6167b5a84197f2a01bf93165fbf4a
Beginneraboutlife116 <pythonforeye@gmail.com>
2023-07-13 14:58:58 +0800

    use different data structure to render

--- src/components/MyPlan/index.tsx
+++ src/components/MyPlan/index.tsx
@@ -3,33 +3,44 @@ import styles from './styles.module.scss';
 
 const dummyData = [
 	{
-		id: 1,
 		storeName: 'xxx 場館',
-		planName: '30堂方案',
-		planType: '次數方案',
-		amountLeft: '5',
+		storeId: 1,
+		plans: [
+			{
+				planName: '30堂方案',
+				planType: '次數方案',
+				amountLeft: 5,
+			},
+			{
+				planName: '30天方案',
+				planType: '天數方案',
+				amountLeft: 25,
+			},
+		],
 	},
 	{
-		id: 2,
 		storeName: 'xxx 場館',
-		planName: '30天方案',
-		planType: '天數方案',
-		amountLeft: '25',
-	},
-	{
-		id: 3,
-		storeName: 'xxx 場館',
-		planName: '8堂方案',
-		planType: '次數方案',
-		amountLeft: '7',
+		storeId: 2,
+		plans: [
+			{
+				planName: '8堂方案',
+				planType: '次數方案',
+				amountLeft: 7,
+			},
+		],
 	},
 ];
 
 export default function MyPlan() {
 	return (
 		<ul className={styles.myPlan}>
-			{dummyData.map((plan) => (
-				<PlanItem key={plan.id} {...plan} />
+			{dummyData.map((data) => (
+				<PlanItem
+					key={data.storeId}
+					storeName={data.storeName}
+					id={data.storeId}
+					plans={data.plans}
+				/>
 			))}
 		</ul>
 	);
